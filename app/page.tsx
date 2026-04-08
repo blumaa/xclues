0ea18d2f83d2989a45542ef8ddd590ca80240569
aaron blum <blumaa@gmail.com>
2026-04-08 12:19:12 +0200

    fix ci

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,10 @@
-import { redirect } from "next/navigation";
-import { headers } from "next/headers";
-import { getGenreFromHost } from "../src/config/domainDetector";
+import { redirect } from 'next/navigation';
+import { headers } from 'next/headers';
+import { getGenreFromHost } from '../src/config/domainDetector';
 
-export default function Home() {
-  redirect("/films");
-
-  export default async function Home() {
-    const headersList = await headers();
-    const host =
-      headersList.get("x-forwarded-host") ??
-      headersList.get("host") ??
-      undefined;
-    const genre = getGenreFromHost(host);
-    redirect(`/${genre}`);
-  }
+export default async function Home() {
+  const headersList = await headers();
+  const host = headersList.get('x-forwarded-host') ?? headersList.get('host') ?? undefined;
+  const genre = getGenreFromHost(host);
+  redirect(`/${genre}`);
 }
