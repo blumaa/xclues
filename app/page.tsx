107f9eec43f8d96a0d91e543aa60c3f69345e6f3
aaron blum <blumaa@gmail.com>
2026-04-08 12:10:26 +0200

    fix domain default

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 import { redirect } from 'next/navigation';
 
+<<<<<<< Updated upstream
 export default function Home() {
   redirect('/films');
+=======
+export default async function Home() {
+  const headersList = await headers();
+  const host = headersList.get('x-forwarded-host') ?? headersList.get('host') ?? undefined;
+  const genre = getGenreFromHost(host);
+  redirect(`/${genre}`);
+>>>>>>> Stashed changes
 }
