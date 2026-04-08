fe4de4d953d25cc0ebbbd0b73526bbb98159ea5b
aaron blum <blumaa@gmail.com>
2026-04-08 12:14:24 +0200

    fix syntax

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
-import { redirect } from 'next/navigation';
-import { headers } from 'next/headers';
-import { getGenreFromHost } from '../src/config/domainDetector';
+import { redirect } from "next/navigation";
+import { headers } from "next/headers";
+import { getGenreFromHost } from "../src/config/domainDetector";
 
 export default function Home() {
-  redirect('/films');
+  redirect("/films");
 
   export default async function Home() {
-  const headersList = await headers();
-  const host = headersList.get('x-forwarded-host') ?? headersList.get('host') ?? undefined;
-  const genre = getGenreFromHost(host);
-  redirect(`/${genre}`);
+    const headersList = await headers();
+    const host =
+      headersList.get("x-forwarded-host") ??
+      headersList.get("host") ??
+      undefined;
+    const genre = getGenreFromHost(host);
+    redirect(`/${genre}`);
+  }
 }
